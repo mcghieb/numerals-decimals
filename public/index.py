40391c6f1061f2b78905f317ce365728375906b2
mcghieb <mcghie.blake@gmail.com>
2023-03-24 19:23:41 -0600

    edit(index.py) all normal descending numerals working

--- public/index.py
+++ public/index.py
@@ -1,23 +1,47 @@
-numers_to_decimals = { 'I' : 1,
-                       'V' : 5,
-                       'X' : 10,
-                       'L' : 50,
-                       'C' : 100,
-                       'D' : 500,
-                       'M' : 1000
-                       }
+numers_dict = { 'I' : 1,
+                'V' : 5,
+                'X' : 10,
+                'L' : 50,
+                'C' : 100,
+                'D' : 500,
+                'M' : 1000
+                }
 
 def convert_numerals(numeral):
     """
     Returns a given Roman Numeral (numeral; passed in as a string) to a Decimal Number.
-    >>> convert_numerals(III)
+    >>> convert_numerals('III')
     3
-    >>> convert_numerals(VII)
+    >>> convert_numerals('VII')
     7
-    >>> convert_numerals(CLXV)
+    >>> convert_numerals('CLXV')
     165
-    >>> convert_numerals(MMMCMXCIX)
+    >>> convert_numerals('MMMCMXCIX')
     3999
     """
-    result = 0
     numer_list = [i for i in numeral]
+
+    def recurse(list_numerals):
+        """
+        helper recursive function for convert_numerals
+        """
+        # base cases
+        if len(list_numerals) == 1:
+            return numers_dict[list_numerals[0]]
+        
+        # normal descending numeral case
+        if numers_dict[list_numerals[0]] >= numers_dict[list_numerals[1]]:
+            return numers_dict[list_numerals[0]] + recurse(list_numerals[1:])
+        
+        # not working yet
+        # elif numers_dict[list_numerals[0]] <= numers_dict[list_numerals[1]]:
+        #     return numers_dict[list_numerals[1]] - numers_dict[list_numerals[0]] + recurse(list_numerals[2:])
+
+    return recurse(numer_list)
+
+# working test cases
+print(convert_numerals('I'))
+print(convert_numerals('CLXV'))
+
+# broken test cases
+# print(convert_numerals('MMMCMXCIX'))

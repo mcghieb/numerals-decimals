78cb57bd97a6b323a11913235f4604e2b6b0c2d9
mcghieb <mcghie.blake@gmail.com>
2023-03-24 21:11:50 -0600

    edit(index.py) minor edits

--- public/index.py
+++ public/index.py
@@ -1,3 +1,5 @@
+#!python3
+
 numers_dict = { 'I' : 1,
                 'V' : 5,
                 'X' : 10,
@@ -6,6 +8,7 @@ numers_dict = { 'I' : 1,
                 'D' : 500,
                 'M' : 1000
                 }
+
 
 def check_valid(numeral):
     """
@@ -67,42 +70,6 @@ def check_valid(numeral):
     return recurse(numer_list)
 
 
-def convert_numerals(numeral):
-    """
-    Returns a given Roman Numeral (numeral; passed in as a string) to a Decimal Number.
-    >>> convert_numerals('III')
-    3
-    >>> convert_numerals('VII')
-    7
-    >>> convert_numerals('CLXV')
-    165
-    >>> convert_numerals('MMMCMXCIX')
-    3999
-    """
-    numer_list = [i for i in numeral]
-
-    def recurse(list_numerals):
-        """
-        helper recursive function for convert_numerals()
-        """
-        # base cases
-        if len(list_numerals) == 1:
-            return numers_dict[list_numerals[0]]
-        if len(list_numerals) < 1:
-            return 0
-        
-        # normal descending numeral case
-        if numers_dict[list_numerals[0]] >= numers_dict[list_numerals[1]]:
-            x = numers_dict[list_numerals[0]]
-            return x + recurse(list_numerals[1:])
-        
-        # other cases
-        elif numers_dict[list_numerals[0]] <= numers_dict[list_numerals[1]]:
-            x = numers_dict[list_numerals[1]] - numers_dict[list_numerals[0]]
-            return x + recurse(list_numerals[2:])
-    
-    return recurse(numer_list)
-
 def parse_type():
     """
     Takes user input, and checks to see if it is a valid type. 
@@ -144,14 +111,66 @@ def parse_type():
             pass
         print(f"\nInvalid input type, please try again.\n")
 
-# def main():
 
-#     print("\nWelcome to Roman Numeral Converter!\nInput either a Number or a Roman Numeral to switch it's type.\n")
-#     while True:
-#         print(f"Enter 'q' to quit.\n")
-#         user_input = parse_type()
+def convert_numerals(numeral):
+    """
+    Returns a given Roman Numeral (numeral; passed in as a string) to a Decimal Number.
+    >>> convert_numerals('III')
+    3
+    >>> convert_numerals('VII')
+    7
+    >>> convert_numerals('CLXV')
+    165
+    >>> convert_numerals('MMMCMXCIX')
+    3999
+    """
+    numer_list = [i for i in numeral]
 
-#         if user_input == 'q':
-#             break
+    def recurse(list_numerals):
+        """
+        helper recursive function for convert_numerals()
+        """
+        # base cases
+        if len(list_numerals) == 1:
+            return numers_dict[list_numerals[0]]
+        if len(list_numerals) < 1:
+            return 0
         
-#     print(f'<<<Exit>>>\nCome back Soon!')
+        # normal descending numeral case
+        if numers_dict[list_numerals[0]] >= numers_dict[list_numerals[1]]:
+            x = numers_dict[list_numerals[0]]
+            return x + recurse(list_numerals[1:])
+        
+        # other cases
+        elif numers_dict[list_numerals[0]] <= numers_dict[list_numerals[1]]:
+            x = numers_dict[list_numerals[1]] - numers_dict[list_numerals[0]]
+            return x + recurse(list_numerals[2:])
+    
+    return recurse(numer_list)
+
+
+def convert_number(number):
+    """
+    Returns a given number (type == int) to a Roman Numeral.
+    >>> convert_numerals(3)
+    III
+    >>> convert_numerals(7)
+    VII
+    >>> convert_numerals(165)
+    CLXV
+    >>> convert_numerals(3999)
+    MMMCMXCIX
+    """
+    return None
+
+def main():
+    print("\nWelcome to Roman Numeral Converter!\nInput either a Number or a Roman Numeral to switch it's type.\n")
+
+    while True:
+        print(f"Enter 'q' to quit.\n")
+        user_input = parse_type()
+
+        if user_input == 'q':
+            break
+        
+    print(f'<<<Exit>>>\nCome back Soon!')
